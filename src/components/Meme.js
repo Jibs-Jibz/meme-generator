892ff30a2d706870c6a38712ccfa3c6cfa9f2a0c
osunkoyajibola <ajibola.osunkoya@thebulb.africa>
2022-07-15 17:17:53 +0100

    pushing to Linux

---
+++ src/components/Meme.js
@@ -0,0 +1,40 @@
+import React from "react";
+import MemesData from "./MemesData"
+
+export default function Meme() {
+
+
+
+    const [memeImage, setmemeImage] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+    const [allMemeImage, setAllMemeImage] = React.useState(MemesData)
+
+    function getmemeImage() {
+        const memesArray = allMemeImage.data.memes
+        const randomNum = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNum].url
+        setmemeImage(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+    }
+
+    return (
+        <section>
+            <div>
+                <div className="input">
+                    <input type="text" className="text-input" placeholder="Top Text" />
+                    <input type="text" className="text-input" placeholder="Bottom Text" />
+                </div>
+                {/* <button type="button" className="button-1">Get a new meme image🖼️</button> */}
+                {/* <a href="#" class="buttonf">Get a new meme image🖼️</a> */}
+                <button class="buttonf" onClick={getmemeImage} >Get a new meme image🖼️</button>
+            </div>
+            <img src={memeImage.randomImage} className="meme-image" />
+        </section>
+
+    )
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+
+import troll from "../img/Troll Face.png"
+
+
+export default function Nav(){
+    return(
+        <nav>
+            <img src={ troll} />
+            <h3>
+                Meme Generator
+            </h3>
+            <p>Done by Jibs</p>
+        </nav>
+    )
+}
